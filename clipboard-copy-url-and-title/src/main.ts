909c52a3a470e6aa796e16c5e82b9ce64acb481c
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-05-02 23:50:06 +0900

    added getting tab infomation

--- clipboard-copy-url-and-title/src/main.ts
+++ clipboard-copy-url-and-title/src/main.ts
@@ -1,10 +1,17 @@
 import App from './App.svelte';
 
+async function getCurrentTabInfo() {
+	let queryOptions = { active: true, currentWindow: true }
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
 const app = new App({
 	target: document.body,
 	props: {
 		name: 'world'
 	}
 });
+
 
 export default app;
