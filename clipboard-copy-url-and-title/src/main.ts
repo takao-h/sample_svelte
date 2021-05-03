03ecd857515f1cdfbd43ef6946a4d3c6d303f3e5
takao-h <hayashi_takao@cyberbuzz.co.jp>
2021-05-03 23:46:09 +0900

    debug

--- clipboard-copy-url-and-title/src/main.ts
+++ clipboard-copy-url-and-title/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.svelte';
 async function getCurrentTabInfo() {
 	let queryOptions = { active: true, currentWindow: true }
   let [tab] = await chrome.tabs.query(queryOptions);
+	console.log(tab);
   return tab;
 }
 
